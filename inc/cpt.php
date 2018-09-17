18bc9f0943e3e6488ea632796d0888797885d1bf
Yan Odnoralov <yan.odnoralov@gmail.com>
2018-09-17 02:51:01 +0200

    events pt

--- inc/cpt.php
+++ inc/cpt.php
@@ -64,6 +64,41 @@ function cptui_register_my_cpts() {
     );
 
     register_post_type( "policyholder_stories", $args );
+    
+    
+    
+	/**
+	 * Post Type: events.
+	 */
+
+	$labels = array(
+		"name" => __( "events", "yan-base" ),
+		"singular_name" => __( "event", "yan-base" ),
+	);
+
+	$args = array(
+		"label" => __( "events", "yan-base" ),
+		"labels" => $labels,
+		"description" => "",
+		"public" => true,
+		"publicly_queryable" => true,
+		"show_ui" => true,
+		"show_in_rest" => false,
+		"rest_base" => "",
+		"has_archive" => false,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"exclude_from_search" => false,
+		"capability_type" => "post",
+		"map_meta_cap" => true,
+		"hierarchical" => false,
+		"rewrite" => array( "slug" => "events", "with_front" => true ),
+		"query_var" => true,
+		"supports" => array( "title", "thumbnail" ),
+	);
+
+	register_post_type( "events", $args );
+
 }
 
 add_action( 'init', 'cptui_register_my_cpts' );
