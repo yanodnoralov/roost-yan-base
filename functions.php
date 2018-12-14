9e8146c2ea704a02a3f7c9975f5e0f7cc1646cf4
Toby Rairdon <tobijah@tobijah.co>
2018-12-13 18:10:09 -0800

    CSS fix

--- functions.php
+++ functions.php
@@ -132,7 +132,7 @@ function yan_base_scripts() {
 	
 	wp_enqueue_style( 'bootstrap',get_template_directory_uri() . '/css/bootstrap.min.css', '5.3.1', true );
 	
-	wp_enqueue_style( 'font-awesome',get_template_directory_uri() . '/css/fontawesome/css/font-awesome-all.css', '5.3.1', true );
+	wp_enqueue_style( 'font-awesome',get_template_directory_uri() . '/css/fontawesome/css/all.css', '5.3.1', true );
 	
 	wp_enqueue_script( 'jquery', get_template_directory_uri() . '/js/jquery-3.3.1.min.js', array(), '1', true );
 
